fcb3190314b38589ec3165a3973840969f499c36
kdnakt <a.kid.1985@gmail.com>
2024-01-07 19:32:57 +0900

    fix biggest implementation

--- pbt/test/prop_base.erl
+++ pbt/test/prop_base.erl
@@ -21,8 +21,15 @@ prop_biggest() ->
 %%%%%%%%%%%%%%%
 boolean(_) -> true.
 
-biggest([Head | _Tail]) ->
-    Head.
+biggest([Head | Tail]) ->
+    biggest(Tail, Head).
+
+biggest([], Biggest) ->
+    Biggest;
+biggest([Head|Tail], Biggest) when Head > Biggest ->
+    biggest(Tail, Head);
+biggest([Head|Tail], Biggest) when Head < Biggest ->
+    biggest(Tail, Biggest).
 
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
