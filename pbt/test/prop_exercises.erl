aeafdbb64e24a00543e6d52c61fd7485d72c1940
kdnakt <a.kid.1985@gmail.com>
2024-02-02 23:54:10 +0900

    A.2.3. problem 3

---
+++ pbt/test/prop_exercises.erl
@@ -0,0 +1,11 @@
+-module(prop_exercises).
+-include_lib("proper/include/proper.hrl").
+
+prop_set_union() ->
+    ?FORALL({ListA, ListB}, {list(number()), list(number())},
+        begin
+            SetA = sets:from_list(ListA),
+            SetB = sets:from_list(ListB),
+            ModelUnion = lists:sort(ListA ++ ListB),
+            lists:sort(sets:to_list(sets:union(SetA, SetB))) =:= ModelUnion
+        end).
