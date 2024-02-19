110b71a2233d8aea670271055baff8d0c517885e
kdnakt <a.kid.1985@gmail.com>
2024-02-19 22:52:21 +0900

    add 3-4 dict_merge

--- pbt/test/prop_exercises.erl
+++ pbt/test/prop_exercises.erl
@@ -9,3 +9,20 @@ prop_set_union() ->
             ModelUnion = lists:usort(ListA ++ ListB),
             lists:sort(sets:to_list(sets:union(SetA, SetB))) =:= ModelUnion
         end).
+
+
+prop_dict_merge() ->
+    ?FORALL({ListA, ListB}, {list({term(), term()}), list({term(), term()})},
+        begin
+            Merged = dict:merge(fun(_Key, V1, _V2) -> V1 end,
+                                dict:from_list(ListA),
+                                dict:from_list(ListB)),
+            extract_keys(lists:sort(dict:to_list(Merged)))
+            ==
+            lists:usort(extract_keys(ListA ++ ListB))
+        end).
+
+% helper
+
+extract_keys(List) -> [K || {K,_} <- List].
+
