48ba0a1d2b8f20e638c0925064776fb5378d4363
kdnakt <a.kid.1985@gmail.com>
2024-09-22 01:28:33 +0900

    add generators

--- bday/test/prop_bday_employee.erl
+++ bday/test/prop_bday_employee.erl
@@ -4,11 +4,27 @@
 % Property
 
 prop_fix_csv_leading_space() ->
-    ?FORALL(MAp, raw_employee_map(),
+    ?FORALL(Map, raw_employee_map(),
         begin
             Emp = bday_employee:adapt_csv_result(Map),
             Strs = [X || X <- maps:keys(Emp) ++ maps:values(Emp), is_list(X)],
             lists:all(fun(STring) -> hd(String) =/= $\s end, Strs)
         end).
+
+% Generator
+
+raw_employee_map() ->
+    ?LET(PropList,
+        [{"last_name", prop_csv:field()},
+         {" first_name", whitespaced_text()},
+         {" date_of_birth", text_date()},
+         {" email", whitespaced_text()}].
+
+whitespaced_text() ->
+    ?LET(Txt, prop_csv:field(), " " ++ Txt).
+
+text_date() ->
+    ?LET({Y,M,D}, {choose(1900,2020), choose(1,12), choose(1,31)},
+        lists:flatten(io_lib:format(" ~w/~2..0w/~2..0w", [Y,M,D]))).
 
 
