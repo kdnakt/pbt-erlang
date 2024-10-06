aeafe8e34d7eaa1922ff8ac89f2d34082f7b694c
kdnakt <a.kid.1985@gmail.com>
2024-10-06 15:15:34 +0900

    update main

--- bday/src/bday.erl
+++ bday/src/bday.erl
@@ -8,10 +8,18 @@
 %%====================================================================
 
 %% escript Entry point
-main(Args) ->
-    io:format("Args: ~p~n", [Args]),
-    erlang:halt(0).
+main([Path]) ->
+    {ok, Data} = file:read_file(Path),
+    Handle = bday_employee:from_csv(binary_to_list(Data)),
+    Query = bday_employee:filter_birthday(Handle, date()),
+    BdaySet = bday_employee:fetch(Query),
+    Mails = [bday_mail_tpl:full(Employee) || Employee <- BdaySet],
+    [send_email(To, Topic, Body) || {To, Topic, Body} <- Mails].
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+send_email(To, Topic, Body) ->
+    io:format("sent birthday email to ~p~n", [To]),
+    io:format("topic: ~p~n", [Topic]),
+    io:format("body: ~p~n", [Body]).
