57799385209ec13f18c50f93b6d233b28e9127ea
kdnakt <a.kid.1985@gmail.com>
2024-12-15 22:35:23 +0900

    add checkout test

---
+++ checkout/test/prop_checkout.erl
@@ -0,0 +1,26 @@
+-module(prop_checkout).
+-include_lib("proper/include/proper.hrl").
+-compile(export_all).
+
+% Property
+prop_no_special1() ->
+    ?FORALL({ItemList, ExpectedPrice, PriceList}, item_price_list(),
+            ExpectedPrice =:= checkout:total(ItemList, PriceList, [])).
+
+% Generator
+item_price_list() ->
+    ?LET(PriceList, price_list(),
+        ?LET({ItemList, ExpectedPrice}, item_list(PriceList),
+            {ItemList, ExpectedPrice, PriceList})).
+
+price_list() ->
+    ?LET(PriceList, non_empty(list({non_empty(string()), integer()})),
+        lists:ukeysort(1, PriceList)). % remove duplicates.
+
+item_list(PriceList) ->
+    ?SIZED(Size, item_list(Size, PriceList, {[], 0})).
+
+item_list(0, _, Acc) -> Acc;
+item_list(N, PriceList, {ItemAcc, PriceAcc}) ->
+    ?LET({Item, Price}, elements(PriceList),
+        item_list(N-1, PriceList, {[Item|ItemAcc], Price+PriceAcc})).
