bf05d3ad260621ab394ae16ac894424fbfd146cc
kdnakt <a.kid.1985@gmail.com>
2024-12-31 20:05:31 +0900

    show metrics

--- checkout/test/prop_checkout.erl
+++ checkout/test/prop_checkout.erl
@@ -7,6 +7,13 @@ prop_no_special1() ->
     ?FORALL({ItemList, ExpectedPrice, PriceList}, item_price_list(),
             ExpectedPrice =:= checkout:total(ItemList, PriceList, [])).
 
+prop_no_special2() ->
+    ?FORALL({ItemList, ExpectedPrice, PriceList}, item_price_list(),
+            collect(
+                bucket(length(ItemList), 10),
+                ExpectedPrice =:= checkout:total(ItemList, PriceList, [])
+            )).
+
 % Generator
 item_price_list() ->
     ?LET(PriceList, price_list(),
@@ -24,3 +31,6 @@ item_list(0, _, Acc) -> Acc;
 item_list(N, PriceList, {ItemAcc, PriceAcc}) ->
     ?LET({Item, Price}, elements(PriceList),
         item_list(N-1, PriceList, {[Item|ItemAcc], Price+PriceAcc})).
+
+bucket(N, Unit) ->
+    (N div Unit) * Unit.
