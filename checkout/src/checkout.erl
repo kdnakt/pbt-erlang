e57d1799968b45d9df28105c776d4d74cd7ab1ad
kdnakt <a.kid.1985@gmail.com>
2024-12-31 19:26:52 +0900

    minimum implementation of total()

--- checkout/src/checkout.erl
+++ checkout/src/checkout.erl
@@ -1,3 +1,10 @@
 -module(checkout).
 
--export([]).
+-export([total/3]).
+
+-type item() :: string().
+-type price() :: integer().
+
+-spec total([item()], [{item(), price()}], any()) -> price().
+total(ItemList, PriceList, _Specials) ->
+    lists:sum([proplists:get_value(Item, PriceList) || Item <- ItemList]).
