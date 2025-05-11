85a3605f7158d212a9f5a983fa199fece137d781
kdnakt <a.kid.1985@gmail.com>
2025-05-12 00:25:55 +0900

    prop_tree_regular

--- target/test/prop_target.erl
+++ target/test/prop_target.erl
@@ -16,10 +16,22 @@ prop_path() ->
             true
         end).
 
+prop_tree_regular(opts) -> [{numtests, 1000}].
+prop_tree_regular() ->
+    ?FORALL_TARGETED(T, tree(),
+        begin
+            Weight = sides(T),
+            io:format(" ~p", [Weight]),
+            true
+        end).
+
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
 %%%%%%%%%%%%%%%
 path() -> list(oneof([left, right, up, down])).
+
+tree() -> 
+    ?LET(L, non_empty(list(integer())), to_tree(L)).
 
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
@@ -44,3 +56,12 @@ insert(N, {leaf, M}) when N > M ->
     {node, N, {leaf, M}, undefined};
 insert(N, undefined) -> 
     {leaf, N}.
+
+sides({node, _, Left, Right}) ->
+    {LL, LR} = sides(Left),
+    {RL, RR} = sides(Right),
+    {count_inner(Left)+LL+LR, count_inner(Right)+RL+RR};
+sides(_) -> {0, 0}.
+
+count_inner({node, _, _, _}) -> 1;
+count_inner(_) -> 0.
