d909001a38d7da572e86f3d5056a332176bab6af
kdnakt <a.kid.1985@gmail.com>
2025-07-29 00:12:31 +0900

    fix compilation error

--- pbt/src/cache.erl
+++ pbt/src/cache.erl
@@ -1,7 +1,7 @@
 -module(cache).
 -export([start_link/1, stop/0, cache/2, find/1, flush/0]).
 -behaviour(gen_server).
-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
 
 start_link(N) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, N, []).
@@ -10,7 +10,7 @@ stop() ->
     gen_server:stop(?MODULE).
 
 init(N) ->
-    ets:new(cache [public, named_table]),
+    ets:new(cache, [public, named_table]),
     ets:insert(cache, {count, 0, N}),
     {ok, nostate}.
 
@@ -22,14 +22,14 @@ handle_info(_Info, State) -> {noreply, State}.
 
 find(Key) ->
     case ets:match(cache, {'_', {Key, '$1'}}) of
-        [[Val]] -> {ok, Val},
+        [[Val]] -> {ok, Val};
         [] -> {error, not_found}
     end.
 
 cache(Key, Value) ->
     case ets:match(cache, {'$1', {Key, '_'}}) of
         [[N]] ->
-            ets:insert(cache, {N, {Key, Value}}),
+            ets:insert(cache, {N, {Key, Value}});
         [] ->
             case ets:lookup(cache, count) of
                 [{count,Max,Max}] ->
