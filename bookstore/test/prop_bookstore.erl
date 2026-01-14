b5a1f436bdb6ff3b5b2d14ce76267010cc09b55b
kdnakt <a.kid.1985@gmail.com>
2026-01-14 23:54:26 +0900

    prop_test()

--- bookstore/test/prop_bookstore.erl
+++ bookstore/test/prop_bookstore.erl
@@ -2,6 +2,45 @@
 -include_lib("proper/include/proper.hrl").
 -compile(export_all).
 
+prop_test() ->
+    ?SETUP(fun() ->
+        {ok, Apps} = application:ensure_all_started(bookstore),
+        fun() -> [application:stop(App) || App <- Apps], ok end
+    end,
+    ?FORALL(Cmds, commands(?MODULE),
+        begin
+            bookstore_db:setup(),
+            {History, State, Result} = run_commands(?MODULE, Cmds),
+            bookstore_db:teardown(),
+            ?WHENFAIL(io:format("History: ~p\nState: ~p\nResult: ~p\n",
+                                [History, State, Result]),
+                                aggregate(command_names(Cmds), Result =:= ok))
+        end)
+    ).
+
+initial_state() -> #{}.
+
+command(_State) ->
+    oneof([
+        {call, bookstore_db, add_book, [isbn(), title(), author(), 1, 1]},
+        {call, bookstore_db, add_book, [isbn()]},
+        {call, bookstore_db, borrow_copy, [isbn()]},
+        {call, bookstore_db, return_copy, [isbn()]},
+        {call, bookstore_db, find_book_by_author, [author()]},
+        {call, bookstore_db, find_book_by_title, [title()]},
+        {call, bookstore_db, find_book_by_isbn, [isbn()]}
+    ]).
+
+precondition(_State, {call, _Mod, _Fun, _Args}) ->
+    true.
+
+postcondition(_State, {call, _Mod, _Fun, _Args}, _Res) ->
+    true.
+
+next_state(State, _Res, {call, _Mod, _Fun, _Args}) ->
+    NewState = State,
+    NewState.
+
 title() ->
     ?LET(S, string(), elements([S, unicode:characters_to_binary(S)])).
 
