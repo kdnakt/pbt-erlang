403d40665576ac1d814ed38a28e054ac3542b8e8
kdnakt <a.kid.1985@gmail.com>
2026-09-24 00:16:44 +0900

    add shim module

---
+++ circuit/test/break_shim.erl
@@ -0,0 +1,28 @@
+-module(break_shim).
+-export([success/0, err/1, ignored_error/1, timeout/0,
+    manual_block/0, manual_deblock/0, manual_reset/0]).
+
+-define(SERVICE, test_service).
+
+success() ->
+    circuit_breaker:call(?SERVICE,
+        fun() -> success end, timer:hours(1),
+        fun() -> true end, timer:hours(1),
+        options()
+    ).
+
+err(Reason) ->
+    circuit_breaker:call(?SERVICE,
+        fun() -> {error, Reason} end, timer:hours(1),
+        fun() -> true end, timer:hours(1),
+        options()
+    ).
+
+ignored_error(Reason) -> err(Reason).
+
+timeout() ->
+    circuit_breaker:call(?SERVICE,
+        fun() -> timer:sleep(infinity) end, 0,
+        fun() -> true end, timer:hours(1),
+        options()
+    ).
